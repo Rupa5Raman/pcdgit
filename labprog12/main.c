6505ef98173f191ace2dd618c3613b75d995405c
Rupa5Raman <rupa05550raman@gmail.com>
2016-10-25 20:10:56 +0530

    Update main.c

--- labprog12/main.c
+++ labprog12/main.c
@@ -1,20 +1,14 @@
-#include<stdio.h>
+ #include<stdio.h>
 #include<conio.h>
-#include<math.h>
-void  main()
+main()
 { 
-float  n,s; int  i;
-clrscr();
-printf("\n  Enter  the  number  to  find  the  square  root\n");
-scanf("%f",&n);
-if(n>0) 
-{ 
-s = n/ 2; 
-for(i = 0; i <n; i++)
-s  =  (s  +  (n/s))/2;
-printf("Square  root  of  %f  is  %f\n",n,s);
-}
-else
-printf("\n  Not  possible  to  find  the  square  root"); 
-getch();
+  int  year;
+  clrscr();
+  printf("Enter  any  year:  ");
+  scanf("%d",&year);
+  if(((year%4==0)&&(year%100!=0))||(year%400==0))
+    printf("%d  is  a  leap  year",year);
+  else
+  printf("%d  is  not  a  leap year",year); 
+  getch();
 }
